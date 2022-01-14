92e2eee58e182c2ed0381aafa85ce2afd9325370
Michal Kubele <michal.kubele@gmail.com>
2022-01-15 00:18:39 +0100

    Add aks configs

--- fraud/build.gradle.kts
+++ fraud/build.gradle.kts
@@ -1,9 +1,9 @@
 
 dependencies {
+    implementation(project(":clients"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-    implementation(project(":clients"))
     implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
     implementation("org.springframework.cloud:spring-cloud-sleuth-zipkin")
     runtimeOnly("org.postgresql:postgresql")
