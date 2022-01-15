2fbce62313a6dd6ceb731106e8b3614f8173239e
mkubele <34385616+mkubele@users.noreply.github.com>
2022-01-15 13:48:44 +0100

    Add api validation (#2)
    * Add api validation
    
    * Update check.yml
    
    * Update check.yml
    
    * Update check.yml
    
    * Add api validation
    
    * Update check.yml
    
    * Fix ktlint

--- customer/src/main/kotlin/com/kubele/customer/CustomerRepository.kt
+++ customer/src/main/kotlin/com/kubele/customer/CustomerRepository.kt
@@ -1,5 +1,14 @@
 package com.kubele.customer
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
-interface CustomerRepository : JpaRepository<Customer, Int>
+interface CustomerRepository : JpaRepository<Customer, Int> {
+    @Query(
+        "SELECT CASE WHEN COUNT(c) > 0 THEN " +
+            "TRUE ELSE FALSE END " +
+            "FROM Customer c " +
+            "WHERE c.email = ?1"
+    )
+    fun selectExistsByEmail(email: String): Boolean
+}
