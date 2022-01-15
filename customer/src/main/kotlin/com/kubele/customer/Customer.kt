407440922362d31812220a1bd970fe5f948d2f65
Michal Kubele <michal.kubele@gmail.com>
2022-01-15 01:18:09 +0100

    Add api validation

--- customer/src/main/kotlin/com/kubele/customer/Customer.kt
+++ customer/src/main/kotlin/com/kubele/customer/Customer.kt
@@ -1,5 +1,6 @@
 package com.kubele.customer
 
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
@@ -11,8 +12,10 @@ data class Customer(
     @Id
     @SequenceGenerator(name = "customer_id_sequence", sequenceName = "customer_id_sequence")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_sequence")
+    @Column(updatable = false)
     val id: Int = 0,
-    val firstName: String,
-    val lastName: String,
-    val email: String
+    val firstName: String = "",
+    val lastName: String = "",
+    @Column(unique = true)
+    val email: String = ""
 )
